1aa8675cd2a453cce33686012e9278c702ab5ff6
edmarsantos <39056557+edmarsantos@users.noreply.github.com>
2021-05-31 21:43:13 -0300

    21.26 - Criando e entendendo a primeira Servlets nessa aula foi visto com utlizar o servlets

--- curso-jsp/src/main/java/beans/BeanCursoJsp.java
+++ curso-jsp/src/main/java/beans/BeanCursoJsp.java
@@ -2,6 +2,75 @@ package beans;
 
 public class BeanCursoJsp {
 	
+	
+	private String login;
+	private String senha;
+	
+	
+	public boolean validarLoginSenha(String login , String senha) {
+		if(login.equalsIgnoreCase("edmar") && senha.equalsIgnoreCase("2606")) {
+			return true;
+		}else
+		{
+			return false;
+		}	
+	}
+	
+	
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+
+
+	private String nome;
+	
+	private String ano;
+	
+	private String sexo;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getAno() {
+		return ano;
+	}
+
+
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+
+
+	public String getSexo() {
+		return sexo;
+	}
+
+
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+
+
 	public int calcula(int numero ) {
 		return numero  * 100;
 	}
